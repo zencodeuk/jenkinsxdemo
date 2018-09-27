546902092e694dd9415432f67838d30c24bca098
zencodeuk <michael.smith@stagecoach-technology.com>
2018-09-27 20:45:09 +0100

    Initial import

---
+++ src/main/java/com/barclays/jenkinsxdemo/DemoApplication.java
@@ -0,0 +1,12 @@
+package com.barclays.jenkinsxdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
